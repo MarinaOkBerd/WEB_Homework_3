88ab7d9a3052d115da7732bdaa995a52c8703ec9
Marina Berdnikova <berdni4ka@rambler.ru>
2023-04-28 17:04:59 +0300

    Add homework 3

--- 1.js
+++ 1.js
@@ -1,5 +1,5 @@
 const a = prompt("Введите температуру в градусах Цельсия: ");
-alert(`Цельсий: ${a}, Фаренгейт: ${get_temp(a)}`);
+alert(`Цельсий: ${a}, Фаренгейт: ${get_temp(a)}.`);
 function get_temp(a) {
     return Math.floor(((9 / 5) * a + 32) * 100) / 100;
 }
